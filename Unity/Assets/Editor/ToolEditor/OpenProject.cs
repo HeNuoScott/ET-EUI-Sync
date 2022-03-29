591e95c08a4c76d5712ea079515bb980312c098c
HeNuoScott <1073498648@qq.com>
2022-03-29 22:44:12 +0800

    解决疑惑问题  新增公司和家代码启动路径

--- Unity/Assets/Editor/ToolEditor/OpenProject.cs
+++ Unity/Assets/Editor/ToolEditor/OpenProject.cs
@@ -6,7 +6,7 @@ namespace ET
 {
     public static class OpenProject
     {
-        [MenuItem("Tools/Open C# Project By VS",false,98)]
+        [MenuItem("Tools/打开代码/客户端工程VS",false,80)]
         public static void OpenProjectVS()
         {
 #if UNITY_EDITOR_OSX
@@ -18,22 +18,45 @@ namespace ET
 #endif
             ProcessHelper.Run(exePath, args);
         }
-        [MenuItem("Tools/Open C# Project By Rider",false,99)]
+        [MenuItem("Tools/打开代码/客户端和服务端Rider",false,81)]
         public static void OpenProjectRider()
         {
+            string exePath = "";
 #if UNITY_EDITOR_OSX
             const string tools = "./Tools";
             UnityEngine.Debug.LogError("暂时没有配置");
 #else
             // 家
-            //const string exePath = @"D:\Application\JetBrains Rider 2021.3.3\bin\rider64.exe";
+            const string exePathHome = @"D:\Application\JetBrains Rider 2021.3.3\bin\rider64.exe";
             // 公司
-            const string exePath = @"C:\Program Files\JetBrains\JetBrains Rider 2021.3.3\bin\rider64.exe";
+            const string exePathCompany = @"C:\Program Files\JetBrains\JetBrains Rider 2021.3.3\bin\rider64.exe";
+
+            string location = EditorPrefs.GetString("office location", "家");
+
+            if (location == "家") exePath = exePathHome;
+            if (location == "公司") exePath = exePathCompany;
             const string args = "..\\Client-Server.sln";
 #endif
             ProcessHelper.Run(exePath, args);
         }
-        
+        [MenuItem("Tools/打开代码/调整工作环境/当前", false, 82)]
+        public static void OfficeLocationCurrent()
+        {
+            string location = EditorPrefs.GetString("office location", "家");
+            UnityEngine.Debug.Log($"当前 工作环境  {location}");
+        }
+        [MenuItem("Tools/打开代码/调整工作环境/家", false, 83)]
+        public static void OfficeLocationHome()
+        {
+            EditorPrefs.SetString("office location", "家");
+            UnityEngine.Debug.Log("工作环境  家  设置成功");
+        }
+        [MenuItem("Tools/打开代码/调整工作环境/公司", false, 84)]
+        public static void OfficeLocationCompany()
+        {
+            EditorPrefs.SetString("office location", "公司");
+            UnityEngine.Debug.Log("工作环境  公司  设置成功");
+        }
         [MenuItem("Tools/打开学习笔记",false,100)]
         public static void OpenProjectXmind()
         {
--- Unity/Codes/Hotfix/Demo/Scene/SceneChangeHelper.cs
+++ Unity/Codes/Hotfix/Demo/Scene/SceneChangeHelper.cs
@@ -5,11 +5,12 @@
         // 场景切换协程
         public static async ETTask SceneChangeTo(Scene zoneScene, string sceneName, long sceneInstanceId)
         {
-            //???????? 为什么删除
-            zoneScene.RemoveComponent<AIComponent>();
+            // 删除zoneScene上的AI组件 之前框架更新的遗留 所以删除
+            // zoneScene.RemoveComponent<AIComponent>();
             
             CurrentScenesComponent currentScenesComponent = zoneScene.GetComponent<CurrentScenesComponent>();
             currentScenesComponent.Scene?.Dispose(); // 删除之前的CurrentScene，创建新的
+            
             Scene currentScene = SceneFactory.CreateCurrentScene(sceneInstanceId, zoneScene.Zone, sceneName, currentScenesComponent);
             UnitComponent unitComponent = currentScene.AddComponent<UnitComponent>();
          
@@ -22,8 +23,9 @@
             Unit unit = UnitFactory.Create(currentScene, m2CCreateMyUnit.Unit);
             unitComponent.Add(unit);
             
-            //???????? 应该是添加
-            zoneScene.RemoveComponent<AIComponent>();
+            // 应该是添加 但是这里并不知道aiConfigId  应该根据sceneInstanceId查询
+            // zoneScene.RemoveComponent<AIComponent>();
+            // zoneScene.AddComponent<AIComponent>(aiConfigId);//因为IAwake<int> 添加AIComponent
             
             Game.EventSystem.Publish(new EventType.SceneChangeFinish() {ZoneScene = zoneScene, CurrentScene = currentScene});
 
